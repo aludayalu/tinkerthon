cb9320e6fe177b32b9f89209dac1cef37c2084ac
aludayalu <pl46u3.ducklinz@gmail.com>
2022-08-25 08:56:09 +0530

    style kro

--- main.py
+++ main.py
@@ -59,30 +59,57 @@ def test():
 
 @app.route('/logout')
 def logout():
-    resp=make_response("logged out")
+    resp=make_response("logged out <script>window.location.href='blog'</script>")
     resp.set_cookie('id', '', expires=0)
     resp.set_cookie('password', '', expires=0)
     return resp
 
 @app.route('/blog')
 def blog():
+    base="""
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400&display=swap" rel="stylesheet">
+    <style>
+    .parav{
+        width:100vw;
+        height:100vh;
+        display: flex;
+        justify-content:center;
+        align-items:center;
+
+    }
+    .card{
+        box-shadow: rgba(99,99,99,0.2) 0px 2px 8px 0px;
+        padding: 1em;
+        font-family:'Lato', sans-serif;
+    }
+    </style>
+    <div class="parav">
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h1 style="font-weight: 700;text-align: center;" class="card-title">_title_</h1>
+    <p class="card-text">_text_</p>
+    </div>
+</div></div>
+    """
     if request.cookies.get('id')==None or request.cookies.get('password')==None or os.path.exists(f'users/{request.cookies.get("id")}')==False:
         return redirect('/')
     args=request.args.to_dict()
     blogs=json.loads(open("blogs.json").read())
     for x in blogs:
         if x["uuid"]==args["id"]:
-            return make_response(x)
+            return make_response(base.replace("_title_",x["title"]).replace("_text_",x["data"]))
     if "eco" in get_user_details(request.cookies.get('id'))[1]:
         blogs=json.loads(open("subjects.json").read())["eco"]
         for x in blogs:
             if x["uuid"]==args["id"]:
-                return make_response(x)
+                return make_response(base.replace("_title_",x["title"]).replace("_text_",x["data"]))
     if "eng" in get_user_details(request.cookies.get('id'))[1]:
         blogs=json.loads(open("subjects.json").read())["eng"]
         for x in blogs:
             if x["uuid"]==args["id"]:
-                return make_response(x)
+                return make_response(base.replace("_title_",x["title"]).replace("_text_",x["data"]))
     return make_response("Couldn't find ur blog")
 
 @app.route('/eco')
@@ -90,14 +117,14 @@ def eco():
     args=request.args.to_dict()
     if request.cookies.get('id')==None or request.cookies.get('password')==None or os.path.exists(f'users/{request.cookies.get("id")}')==False or "eco" not in get_user_details(request.cookies.get('id'))[1]:
         return make_response("Unauth Access")
-    layout='<link rel="stylesheet" href="blog.css">'
-    base='<div class="allposts"> <div class="card"> <h2 class="posttitle">title</h2> <div class="author">uname</div><div class="article"> data </div><div class="readmore"> <a href="#" class="open" >Read More</a> </div></div></div>'
+    layout='<link rel="stylesheet" href="blog.css"><div class"allposts" style="display:flex;justify-content:space-evenly;align-items:center;flex-wrap:wrap">'
+    base='<div class="card"> <h2 class="posttitle">.title.</h2> <div class="author" style="text-align: center;">uname</div><div class="article"> data </div><div class="readmore"> <a href="#" class="open" >Read More</a> </div></div> '
     if request.cookies.get('id')==None or request.cookies.get('password')==None or os.path.exists(f'users/{request.cookies.get("id")}')==False:
         return redirect('/')
     blogs=json.loads(open("subjects.json").read())["eco"]
     blogs.reverse()
     for x in blogs:
-        layout+=base.replace("uname",f"By : {x['uploader']}").replace("data",x["data"]).replace("title",x["title"]).replace("#",f'http://127.0.0.1:5000/blog?id={x["uuid"]}')
+        layout+=base.replace("uname",f"@{x['uploader']}").replace("data",x["data"]).replace(".title.",x["title"]).replace("#",f'http://127.0.0.1:5000/blog?id={x["uuid"]}')
     return make_response(layout)
 
 @app.route('/eng')
@@ -105,14 +132,14 @@ def eng():
     args=request.args.to_dict()
     if request.cookies.get('id')==None or request.cookies.get('password')==None or os.path.exists(f'users/{request.cookies.get("id")}')==False or "eng" not in get_user_details(request.cookies.get('id'))[1]:
         return make_response("Unauth Access")
-    layout='<link rel="stylesheet" href="blog.css">'
-    base='<div class="allposts"> <div class="card"> <h2 class="posttitle">title</h2> <div class="author">uname</div><div class="article"> data </div><div class="readmore"> <a href="#" class="open" >Read More</a> </div></div></div>'
+    layout='<link rel="stylesheet" href="blog.css"><div class"allposts" style="display:flex;justify-content:space-evenly;align-items:center;flex-wrap:wrap">'
+    base='<div class="card"> <h2 class="posttitle">.title.</h2> <div class="author" style="text-align: center;">uname</div><div class="article"> data </div><div class="readmore"> <a href="#" class="open" >Read More</a> </div></div> '
     if request.cookies.get('id')==None or request.cookies.get('password')==None or os.path.exists(f'users/{request.cookies.get("id")}')==False:
         return redirect('/')
     blogs=json.loads(open("subjects.json").read())["eng"]
     blogs.reverse()
     for x in blogs:
-        layout+=base.replace("uname",f"By : {x['uploader']}").replace("data",x["data"]).replace("title",x["title"]).replace("#",f'http://127.0.0.1:5000/blog?id={x["uuid"]}')
+        layout+=base.replace("uname",f"@{x['uploader']}").replace("data",x["data"]).replace(".title.",x["title"]).replace("#",f'http://127.0.0.1:5000/blog?id={x["uuid"]}')
     return make_response(layout)
 
 @app.route('/upload')
@@ -138,13 +165,40 @@ def blog_general_upload():
 @app.route('/blogs')
 def blogs():
     layout='<link rel="stylesheet" href="blog.css">'
-    base='<div class="allposts"> <div class="card"> <h2 class="posttitle">title</h2> <div class="author">uname</div><div class="article"> data </div><div class="readmore"> <a href="#" class="open" >Read More</a> </div></div></div>'
+    layout+="""
+    <div class="w3-top">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <style>
+    .w3-wide {letter-spacing: 10px;}
+    .w3-hover-opacity {cursor: pointer;}
+  </style>
+  <div class="w3-bar" id="myNavbar">
+    <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+    </a>
+    <a href="/blogs" class="w3-bar-item w3-button" onclick="toggleFunction()"><p>BLOGS</p></a>
+    <a href="/panel" class="w3-bar-item w3-button" onclick="toggleFunction()"><p>UPLOAD</p></a>
+    <a href="/oilspill" class="w3-bar-item w3-button" onclick="toggleFunction()"><p>OIL SPILLS</p></a>
+    <a href="/plastic" class="w3-bar-item w3-button" onclick="toggleFunction()"><p>PLASTIC POLLUTION</p></a>
+    <a href="/marine_life" class="w3-bar-item w3-button" onclick="toggleFunction()"><p>MARINE LIFE</p></a>
+    <a href="/logout" class="w3-bar-item w3-button" onclick="toggleFunction()"><p>LOGOUT</p></a>
+  </div>
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+    <a href="/blogs" class="w3-bar-item w3-button" onclick="toggleFunction()">BLOGS</a>
+    <a href="/logout" class="w3-bar-item w3-button" onclick="toggleFunction()">LOGOUT</a>
+  </div>
+</div>
+<p style="height:30px;"></p>
+    """
+    layout+='<div class"allposts" style="display:flex;justify-content:space-evenly;align-items:center;flex-wrap:wrap;">'
+    base='<div class="card"> <h2 class="posttitle">.title.</h2> <div class="author" style="text-align: center;">uname</div><div class="article"> data </div><div class="readmore"> <a href="#" class="open" >Read More</a> </div></div> '
     if request.cookies.get('id')==None or request.cookies.get('password')==None or os.path.exists(f'users/{request.cookies.get("id")}')==False:
         return redirect('/')
     blogs=json.loads(open("blogs.json").read())
     blogs.reverse()
     for x in blogs:
-        layout+=base.replace("uname",f"By : {x['uploader']}").replace("data",x["data"]).replace("title",x["title"]).replace("#",f'http://127.0.0.1:5000/blog?id={x["uuid"]}')
+        layout+=base.replace("uname",f"@{x['uploader']}").replace("data",x["data"]).replace(".title.",x["title"]).replace("#",f'http://127.0.0.1:5000/blog?id={x["uuid"]}')
+    layout+="</div>"
     return make_response(layout)
 
 @app.route('/panel')
@@ -158,5 +212,15 @@ def give_blogcss():
 @app.route("/landing.css")
 def give_landingcss():
     return make_response(open("templates/landing.css").read())
+
+@app.route('/marine_life')
+def marine():
+    return make_response(open("templates/marinelife.html").read())
+@app.route("/oilspill")
+def oil():
+    return render_template("oilspill.html")
+@app.route("/plastic")
+def plastic():
+    return render_template("plasticpollution.html")
 
 app.run(debug=True)
